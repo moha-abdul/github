1c6f3f5f4fd2bed7ae88f1d7e48df858ac05f4ee
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-17 12:46:34 +0300

    Generate github-repo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
 import { GithubSearchComponent } from './github-search/github-search.component';
 import { GithubUserComponent } from './github-user/github-user.component';
+import { GithubRepoComponent } from './github-repo/github-repo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubComponent,
     GithubSearchComponent,
-    GithubUserComponent
+    GithubUserComponent,
+    GithubRepoComponent
   ],
   imports: [AlertModule.forRoot(),
     BrowserModule
