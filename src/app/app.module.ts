76c2a47250c23ec555a0f8800bb06a46d4202c2a
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-17 12:45:10 +0300

    Generate github-user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
 import { GithubSearchComponent } from './github-search/github-search.component';
+import { GithubUserComponent } from './github-user/github-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubComponent,
-    GithubSearchComponent
+    GithubSearchComponent,
+    GithubUserComponent
   ],
   imports: [AlertModule.forRoot(),
     BrowserModule
