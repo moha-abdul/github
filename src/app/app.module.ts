1966add13b27f456317b1656eae677c5e8daea98
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-17 12:12:51 +0300

    Generate github-search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
+import { AlertModule } from 'ngx-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GithubComponent } from './github/github.component';
+import { GithubSearchComponent } from './github-search/github-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubComponent,
+    GithubSearchComponent
   ],
-  imports: [
+  imports: [AlertModule.forRoot(),
     BrowserModule
   ],
   providers: [],
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,4 +1,4 @@
 export class User {
-  constructor(public username: string, public repository: ) {
-  }
+  // constructor(public username: string, public repository: ) {
+  // }
 }
